6c843f2ccef611620721aa65b3eb26391e5b5cc5
AidanHassett <aidanhassett.personal@gmail.com>
2022-02-15 15:30:52 +0000

    Updated Customer.java to include email field

--- src/main/java/com/qa/ims/persistence/domain/Customer.java
+++ src/main/java/com/qa/ims/persistence/domain/Customer.java
@@ -3,10 +3,21 @@ package com.qa.ims.persistence.domain;
 public class Customer {
 
 	private Long id;
+	private String email;
 	private String firstName;
 	private String surname;
 
+	public Customer(String email) {
+		this.setEmail(email);
+	}
+
 	public Customer(String firstName, String surname) {
+		this.setFirstName(firstName);
+		this.setSurname(surname);
+	}
+
+	public Customer(String email, String firstName, String surname) {
+		this.setEmail(email);
 		this.setFirstName(firstName);
 		this.setSurname(surname);
 	}
@@ -23,6 +34,14 @@ public class Customer {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getFirstName() {
@@ -43,7 +62,7 @@ public class Customer {
 
 	@Override
 	public String toString() {
-		return "id:" + id + " first name:" + firstName + " surname:" + surname;
+		return "id:" + id + " email:" + email + " first name:" + firstName + " surname:" + surname;
 	}
 
 	@Override
@@ -53,6 +72,7 @@ public class Customer {
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		return result;
 	}
 
@@ -65,21 +85,31 @@ public class Customer {
 		if (getClass() != obj.getClass())
 			return false;
 		Customer other = (Customer) obj;
+
+		if (getId() == null) {
+			if (other.getId() != null)
+				return false;
+		} else if (!id.equals(other.getId()))
+			return false;
+
+		if (getEmail() == null) {
+			if (other.getEmail() != null)
+				return false;
+		} else if (!getEmail().equals(other.getEmail()))
+			return false;
+
 		if (getFirstName() == null) {
 			if (other.getFirstName() != null)
 				return false;
 		} else if (!getFirstName().equals(other.getFirstName()))
 			return false;
-		if (id == null) {
-			if (other.id != null)
+
+		if (getSurname() == null) {
+			if (other.getSurname() != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!getSurname().equals(other.getSurname()))
 			return false;
-		if (surname == null) {
-			if (other.surname != null)
-				return false;
-		} else if (!surname.equals(other.surname))
-			return false;
+			
 		return true;
 	}
 
